c62ccb30409c045d592af504958574315b96b78b
lanmower <almagestfraternite@gmail.com>
2024-09-27 07:40:12 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #cd mmpose
 cd /mmpose/projects/rtmpose3d/
+wget -O /mmpose/projects/rtmpose3d/entrypoint.py https://raw.githubusercontent.com/lanmower/3d-tracker/main/docker/entrypoint.py
 python entrypoint.py
